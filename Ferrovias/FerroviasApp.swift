1b2b7e785182e44e062bab0dbb6b6477d5c11cbf
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2024-05-17 18:00:41 -0300

    Initial Commit

---
+++ Ferrovias/FerroviasApp.swift
@@ -0,0 +1,17 @@
+//
+//  FerroviasApp.swift
+//  Ferrovias
+//
+//  Created by Federico Diaz Nemeth on 17/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct FerroviasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
