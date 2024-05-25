151aa1666d8bd19cf52423d4071d2410bf57d84c
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2024-05-25 20:56:34 -0300

    feat(watchOS): Added first version for watchOS

---
+++ WFerrovias Watch App/WFerroviasApp.swift
@@ -0,0 +1,17 @@
+//
+//  WFerroviasApp.swift
+//  WFerrovias Watch App
+//
+//  Created by Federico Diaz Nemeth on 25/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WFerrovias_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
